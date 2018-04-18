b4459d36ee97efc8c68684dcb8cb09b0f9b38e4d
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-04-18 00:27:12 -0400

    start from dls 2017

---
+++ nsf_pi_18/auto/slides.el
@@ -0,0 +1,164 @@
+(TeX-add-style-hook
+ "slides"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("xy" "all")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer12"
+    "fontenc"
+    "garamond"
+    "multicol"
+    "pifont"
+    "etex"
+    "graphicx"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "semantic"
+    "xy"
+    "color"
+    "listings"
+    "fancybox"
+    "stmaryrd"
+    "rotating"
+    "wasysym"
+    "ulem"
+    "enumitem")
+   (TeX-add-symbols
+    '("hy" 3)
+    '("meet" 2)
+    '("join" 2)
+    '("case" 3)
+    '("new" 1)
+    '("CAST" 1)
+    '("ftv" 1)
+    '("all" 1)
+    '("mkcast" 1)
+    '("cast" 1)
+    '("pblame" 2)
+    '("blame" 1)
+    '("cod" 1)
+    '("dom" 1)
+    '("Lam" 1)
+    '("lam" 1)
+    '("key" 1)
+    '("val" 2)
+    '("rtti" 2)
+    '("RefC" 1)
+    '("bcfun" 1)
+    '("pl" 1)
+    '("qu" 2)
+    '("FAIL" 3)
+    '("Fail" 1)
+    '("fail" 3)
+    '("Id" 1)
+    '("rng" 1)
+    '("lo" 1)
+    '("safe" 1)
+    '("static" 2)
+    '("labels" 1)
+    '("kw" 1)
+    '("pr" 1)
+    '("gr" 1)
+    '("rd" 1)
+    '("bl" 1)
+    '("embed" 1)
+    '("labeltag" 1)
+    '("feat" 1)
+    '("Rbox" 1)
+    '("Gbox" 1)
+    "featstart"
+    "featend"
+    "Topcircle"
+    "BOTTOMCIRCLE"
+    "halfcircle"
+    "featY"
+    "featP"
+    "featN"
+    "type"
+    "typeS"
+    "typeG"
+    "typeCC"
+    "evall"
+    "evallS"
+    "evallG"
+    "evallCC"
+    "evallD"
+    "reduce"
+    "becomes"
+    "EE"
+    "FF"
+    "Hole"
+    "divergeG"
+    "subtype"
+    "consis"
+    "semi"
+    "id"
+    "query"
+    "pling"
+    "MergeT"
+    "error"
+    "Obj"
+    "String"
+    "Double"
+    "ba"
+    "ea"
+    "dotspace"
+    "Base"
+    "dyn"
+    "Dyn"
+    "Int"
+    "Float"
+    "Bool"
+    "Str"
+    "Ref"
+    "by"
+    "app"
+    "tapp"
+    "of"
+    "tu"
+    "To"
+    "Let"
+    "Letrec"
+    "In"
+    "If"
+    "Then"
+    "Else"
+    "True"
+    "False"
+    "as"
+    "op"
+    "ledyn"
+    "IS"
+    "alloc"
+    "deref"
+    "update"
+    "oldmacro")
+   (LaTeX-add-labels
+    "#1")
+   (LaTeX-add-environments
+    "stack"
+    "branch"
+    "syntax")
+   (LaTeX-add-listings-lstdefinestyles
+    "basic")
+   (LaTeX-add-amsthm-newtheorems
+    "conjecture"
+    "proposition"))
+ :latex)
+
---
+++ nsf_pi_18/plots/README.md
@@ -0,0 +1,15 @@
+# Plots
+
+Put the plots of the performance results in this directory.
+
+
+1. external comparisons for static languages
+
+2. external comparisons for dynamic languages
+
+3. partial typing internal experiments
+
+    a) type-based vs. coercions (space efficient)
+	b) unspecialized vs. specialized for proxied coercions
+	c) monotonic vs. proxied for coercions
+	e) eager vs. lazy for monotonic coercions
